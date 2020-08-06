b95e12b1349f06853195260bfd99a49c52bacb9d
Kaan Çelen <berat.kaan.celen@accenture.com>
2020-08-06 16:59:32 +0300

    ret

--- pvvm/src/main/java/com/kaancelen/pvvm/network/RetrofitService.kt
+++ pvvm/src/main/java/com/kaancelen/pvvm/network/RetrofitService.kt
@@ -1,2 +1,18 @@
 package com.kaancelen.pvvm.network
 
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitService(private val baseUrl: String, private val okHttpService: OkHttpService) {
+
+    fun newRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpService.newOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+}
